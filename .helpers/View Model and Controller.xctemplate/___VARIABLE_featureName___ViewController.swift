fd746faf1cdc691aad34b4a09501702dc0085290
Swain Molster <swain.molster@gmail.com>
2018-07-22 15:43:34 -0400

    Added helper template for ViewModel and Controller

---
+++ .helpers/View Model and Controller.xctemplate/___VARIABLE_featureName___ViewController.swift
@@ -0,0 +1,14 @@
+// ___FILEHEADER___
+
+import UIKit
+
+final internal class ___FILEBASENAME___: UIViewController {
+    
+    private let viewModel: ___VARIABLE_featureName___ViewModelType = ___VARIABLE_featureName___ViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
---
+++ .helpers/View Model and Controller.xctemplate/___VARIABLE_featureName___ViewModel.swift
@@ -0,0 +1,28 @@
+// ___FILEHEADER___
+
+import Foundation
+import FitnessTrackerKit
+import ReactiveSwift
+
+internal protocol ___FILEBASENAME___Inputs {
+    
+}
+
+internal protocol ___FILEBASENAME___Outputs {
+    
+}
+
+internal protocol ___FILEBASENAME___Type {
+    var inputs: ___FILEBASENAME___Inputs { get }
+    var outputs: ___FILEBASENAME___Outputs { get }
+}
+
+internal final class ___FILEBASENAME___: ___FILEBASENAME___Type, ___FILEBASENAME___Inputs, ___FILEBASENAME___Outputs {
+    
+    // MARK: - Inputs
+    
+    // MARK: - Outputs
+    
+    var inputs: ___FILEBASENAME___Inputs { return self }
+    var outputs: ___FILEBASENAME___Outputs { return self }
+}
